3f0f3db027d119c681054bacb1a4641d3e3f1668
omar zinane <zinaneomar2000@gmail.com>
2024-06-21 17:22:30 +0100

    first commit

---
+++ src/app/devis/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Devis() {
+  return (
+    <div className="h-[100vh] bg-slate-300 container pt-24 lg:pt-28">
+      <p className="text-zinc-950">devis</p>
+    </div>
+  );
+}
+
+export default Devis;
