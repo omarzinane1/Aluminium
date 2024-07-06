2765acec28e550cbe97e77157884e09b30dd95e4
omar zinane <zinaneomar2000@gmail.com>
2024-07-06 19:53:17 +0100

    manipulation de composant Process  19:25  06/07/2024

--- src/app/devis/page.tsx
+++ src/app/devis/page.tsx
@@ -4,6 +4,7 @@ function Devis() {
   return (
     <div className="h-[100vh] bg-slate-300 container pt-24 lg:pt-28">
       <h1 className="text-zinc-950">NOTRE SOCIÉTÉ</h1>
+      <p></p>
     </div>
   );
 }
